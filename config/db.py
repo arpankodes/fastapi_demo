693f32cab06742b49a6b8499ae4ac72bab09d8a1
arpankodes <arpan.5721@gmail.com>
2022-02-18 11:19:25 +0530

    db created and configured

--- config/db.py
+++ config/db.py
@@ -1,4 +1,13 @@
-from venv import create
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql://")
+engine = create_engine("mysql+pymysql://root:Favno.666@localhost:3306/fastapidemo")
+
+# ref for sql db configuration
+# https://fastapi.tiangolo.com/tutorial/sql-databases/
+#
+#
+# PyMySQL is a pure-Python MySQL client library
+# ref: https://zetcode.com/python/pymysql/ 
+
+conn = engine.connect()
+meta = MetaData()
